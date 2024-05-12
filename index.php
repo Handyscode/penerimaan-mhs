1df50bd4047c799f867732d3b29acca0e4128fbc
Handyscode <handyshan12@gmail.com>
2024-05-12 20:17:46 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Website Penerimaan Mahasiswa Baru</title>
+  <link rel="stylesheet" href="assets/css/style.css">
+
+  <!-- Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+</head>
+<body>
+  <header class="header">
+    <div class="navbrand">
+      <a href="">Penerimaan Mahasiswa</a>
+    </div>
+    <nav class="navbar">
+      <ul>
+        <li>
+          <a href="/">Beranda</a>
+        </li>
+        <li>
+          <a href="/form-daftar.php">Form Pendaftaran</a>
+        </li>
+      </ul>
+    </nav>
+    <div class="action-btn">
+      <a href="/login.php" class="nav-link primary-btn">Login</a>
+      <a href="/register.php" class="nav-link primary-btn-outlined">Daftar</a>
+    </div>
+  </header>
+
+  <div class="container">
+    <section class="hero-section">
+      <h1 class="headline">Penerimaan Mahasiswa Baru</h1>
+      <p class="subheadline">Tahun ajaran 2025-2026</p>
+      <div class="action-btn">
+        <a href="/form-daftar.php" class="main-cta primary-btn">Daftar Sekarang</a>
+      </div>
+    </section>
+  </div>
+
+  <footer class="footer">
+    <p class="copyright">Copyright &copy; 2024 - Penerimaan Mahasiswa Baru</p>
+  </footer>
+</body>
+</html>
